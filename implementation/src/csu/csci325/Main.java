048f136b1b39f73fc8497c54f80373f3771210d8
BrBrake <bcbrake@csustudent.net>
2015-11-29 16:40:38 -0500

    user profile

--- implementation/src/csu/csci325/Main.java
+++ implementation/src/csu/csci325/Main.java
@@ -3,6 +3,14 @@ package csu.csci325;
 public class Main {
     public static void main(String[] args) {
         StartFrame sf = new StartFrame();
+        AddUsers added = new AddUsers();
+        //Users user1 = new Users("Brian", "cat");
+
+        added.put("Brian", "cat");
+        added.put("N'dia", "java");
+
+        System.out.println(added.size());
+        System.out.println(added.get("java"));
 
 
     //NEED TO SWITCH FRAMES TO PANELS CALLED FROM START FRAME
